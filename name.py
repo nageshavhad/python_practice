49a67901ee15fd52678f5f2512e5df368c3282cd
Nagesh Avhad <menageshavhad@gmail.com>
2024-06-15 10:59:13 +0530

    signal

--- name.py
+++ name.py
@@ -1,5 +1,5 @@
 name = input("name :")
 age = int(input("age :"))
-price = float(input(" price :"))
+price = float(input(" price:"))
 
 print("my name is", name, "and i am", age, "years old")
---
+++ signal.py
@@ -0,0 +1,10 @@
+light = input("light color : ")
+
+if(light == "red"):
+    print("stop")
+elif(light == "yelow"):
+    print("look")
+elif(light =="green"):
+    print("go")
+else:
+    print("light is broken")
